9f0283c928aa560f2385ece8aeb561d2a09ef34d
octofox1357 <octofox1357@gmail.com>
2021-09-14 15:30:52 +0900

    get images as array

--- server/server.py
+++ server/server.py
@@ -16,7 +16,7 @@ model = None  # 인공지능 변수 전역에 선언
 
 
 class Base64Image(BaseModel):
-    base64 : str
+    strData : str
 
 @app.on_event('startup')  # 서버 시작과 함께 인공지능 모델을 불러옴
 async def load_model():
@@ -43,28 +43,37 @@ async def main():
 
 # https://stackoverflow.com/questions/61333907/receiving-an-image-with-fast-api-processing-it-with-cv2-then-returning-it
 @app.post("/predict/skin/oil")
-async def predict_skin_oil(file: UploadFile = File(...)):
-    contents = await file.read()
-    npArr = np.fromstring(contents, np.uint8)
-    img = cv2.imdecode(npArr, cv2.IMREAD_GRAYSCALE)
-    img = np.array(cv2.resize(img, dsize=(256, 256),
-                              interpolation=cv2.INTER_AREA))
-    img = img.reshape(-1, 256*256)
-    global model
-    model.predict(img)
-    return {"result": str(model.predict(img)[0])}
-
-
-@app.post("/predict/skin/oil/base64")
-def skin(imgStr: Base64Image):
-    if(imgStr.base64 == None):
+async def predict_skin_oil(files: List[UploadFile] = File(...)):
+    if(files == None):
         return {
             "result": False,
             "Error": "Data input required"
         }
-    img_data = base64.b64decode(imgStr.base64)
-    image = Image.open(io.BytesIO(img_data))
-    img = cv2.cvtColor(np.array(image), cv2.IMREAD_GRAYSCALE)
+    resArr = [];
+    for file in files:
+        contents = await file.read()
+        npArr = np.fromstring(contents, np.uint8)
+        img = cv2.imdecode(npArr, cv2.IMREAD_GRAYSCALE)
+        img = np.array(cv2.resize(img, dsize=(256, 256),
+                              interpolation=cv2.INTER_AREA))
+        img = img.reshape(-1, 256*256)
+        global model
+        resArr.append(str(model.predict(img)[0]))
+
+    return {"result": resArr}
+
+
+@app.post("/predict/skin/oil/base64")
+def skin(imgStr: Base64Image):
+    if(imgStr.strData == None):
+        return {
+            "result": False,
+            "Error": "Data input required"
+        }
+    img_data = base64.b64decode(imgStr.strData)
+    npimg = np.fromstring(img_data, dtype=np.uint8);
+    source = cv2.imdecode(npimg, 1)
+    img = cv2.cvtColor(source, cv2.IMREAD_GRAYSCALE)
     img = np.array(cv2.resize(img, dsize=(256, 256),
                               interpolation=cv2.INTER_AREA))
     img = img.reshape(-1, 256*256)
